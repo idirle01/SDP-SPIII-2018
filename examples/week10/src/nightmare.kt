d24b8df5b9d614fdff45aa5cba16823e93e64029
Keith L Mannock <keith@dcs.bbk.ac.uk>
2018-03-13 16:09:12 +0000

    cycles example

--- examples/week10/src/nightmare.kt
+++ examples/week10/src/nightmare.kt
@@ -1,29 +1,59 @@
 typealias IntFunction = (Int) -> Int
 
-fun cycle(f1: IntFunction, f2: IntFunction, f3: IntFunction): IntFunction {
-    return { x -> f1(x)}
-}
+fun cycle(f1: IntFunction, f2: IntFunction, f3: IntFunction): (Int) -> IntFunction =
+        { n: Int ->
+            { x: Int ->
+                // var i = 0
+                // while i < n do
+                fun loop(i: Int, x: Int): Int =
+                        if (i < n) {
+                            val rem = i % 3
+                            when (rem) {
+                                0 -> loop(i + 1, f1(x))
+                                1 -> loop(i + 1, f2(x))
+                                else -> loop(i + 1, f3(x))
+                            }
+                        } else {
+                            x
+                        }
+
+                loop(0, x)
+            }
+        }
 
 fun add1(x: Int) = x + 1
-fun times2(x : Int) = x * 2
+fun times2(x: Int) = x * 2
 fun add3(x: Int) = x + 3
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
 //    println(cycle(::add1, ::times2, ::add3)(5))
-    val myCycle = cycle(::add1,::times2,::add3)
+    val myCycle = cycle(::add1, ::times2, ::add3)
     val id = myCycle(0)
-    print(id(5))
+    println(id(5))
     // prints 5
 
     val addOneThenDouble = myCycle(2)
-    print(addOneThenDouble(1))
+    println(addOneThenDouble(1))
     // prints 4
 
     val doAllFunctions = myCycle(3)
-    print(doAllFunctions(2))
+    println(doAllFunctions(2))
     // prints 9
+
+    val doMoreThanACycle = myCycle(4)
+    println(doMoreThanACycle(2))
+    // prints 10
+
+    val doTwoCycles = myCycle(6)
+    println(doTwoCycles(1))
+    // prints 19
+
 }
 
-/*
-
- */
+//
+//                when (n) {
+//                    1 -> f1(x)
+//                    2 -> f2(f1(x))
+//                    3 -> f3(f2(f1(x)))
+//                    else -> x
+//                }
